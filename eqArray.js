9821994bdcb43fe7c4dfcf5d6acb677b1a98d0e5
SlavaRez25 <reznik.slava@gmail.com>
2023-02-09 02:07:47 +0000

    Now linted

--- eqArray.js
+++ eqArray.js
@@ -12,28 +12,28 @@ const assertEqual = function(actual, expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } 
+  }
 
 };
 
 const eqArrays = function(firstArr, secondArr) {
-  if (firstArr.length !== secondArr.length){
+  if (firstArr.length !== secondArr.length) {
     return false;
   }
-  for (let i = 0; i < firstArr.length; i++){
-    if (firstArr[i] !== secondArr[i]){
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
       return false;
     }
-}
-return true;
-}
+  }
+  return true;
+};
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 
 
 
