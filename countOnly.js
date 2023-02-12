3dd9763343024537ff522338382227abdf2aa16a
SlavaRez25 <reznik.slava@gmail.com>
2023-02-12 17:59:20 +0000

    Fixed the function

--- countOnly.js
+++ countOnly.js
@@ -7,17 +7,32 @@ const assertEqual = function(actual, expected) {
  } else {
    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
  }
- 
 };
 
-
 const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
+  for (const item of allItems) {
+    // console.log(item);
+    if (itemsToCount[item] === true){
+     if (results[item]) {
+        results[item] += 1;
+      } else {
+       results[item] = 1;
+      }
+   }
+  } 
+  // console.log(results);
+  return results;
 }
 
+const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
 
-const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe";]
+const result1 = countOnly(firstNames, { "Jason": true, "Karina": true, "Fang": true, "Agouhanna": false });
 
-const result1 = countOnly(firstNames, { "Jason": true})
 
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karina"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
 
