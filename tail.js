34d770fe09867589d154386f5129d4d8f0fac5c3
SlavaRez25 <reznik.slava@gmail.com>
2023-03-03 01:27:46 +0000

    refactored tail

--- tail.js
+++ tail.js
@@ -1,24 +1,14 @@
 // Function Implementation
 
-const assertEqual = function(actual, expected) {
-  if (typeof actual === 'string' && typeof expected === 'string' && actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (typeof actual === 'string' && typeof expected === 'string' && actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  } else if (typeof actual === 'number' && typeof expected === 'number' && actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-  
-};
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
 
 //Tail function
 
 const tail = function(arr) {
   let newArray = arr.slice(1);
-  console.log(arr);
-  console.log(newArray);
+  // console.log(arr);
+  // console.log(newArray);
   return newArray;
 };
 
@@ -27,14 +17,8 @@ const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
+assertArraysEqual(tail(["This", "is", "only", "three"]), ["is", "only", "three"]);
 
-//Test code assertEqueal
-
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(12, 1);
-assertEqual('bootcamp', 'bootcamp');
-assertEqual('bootcamp', 'bootcampers');
 
 
 
