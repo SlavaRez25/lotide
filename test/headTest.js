c8797b9d2e76b1c67b665c3f2c2f1cd68d698fa7
Slava <reznik.slava@gmail.com>
2023-03-09 18:45:14 +0000

    updated with Mocha & Chai~

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,24 @@
 
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
 
-// Head test code
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([225,226,227]), 5);
-assertEqual(head(['blue', 'yellow', 'red']), 'red');
-assertEqual(head([]), 5);
-assertEqual(head([1]), 1);
-assertEqual(head([2]), 5);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns Hello for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello");
+  });
+});
+
+
+
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
+
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,30 @@
 
-const assertEqual = require('../assertEqual');
-const assertArraysEqual = require('../assertArraysEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail = require('../tail')
+
+describe("#tail", () => {
+  it("returns is only three for [This is only three]", () => {
+    assert.deepEqual(tail(["This", "is", "only", "three"]), ["is", "only", "three"]);
+  });
+
+  it("returns 2, 3, 4, 5 for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+});
 
 
-// Test Case: Check the original array
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-assertArraysEqual(tail(["This", "is", "only", "three"]), ["is", "only", "three"]);
+// const assertEqual = require('../assertEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+// const tail = require('../tail');
+
+
+// // Test Case: Check the original array
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// assertArraysEqual(tail(["This", "is", "only", "three"]), ["is", "only", "three"]);
 
